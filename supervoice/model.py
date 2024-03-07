fix: fix f0 normalization

supervoice/model.py
@@ -42,8 +42,8 @@ class SuperVoice(torch.nn.Module):
         styles = []
         for t in alignments:
             for i in range(t[1]):
-                phonemes.append(t[0]) # Already padded by default
-                styles.append(t[2] + 1) # Style tokens are not padded by default
+                phonemes.append(t[0])
+                styles.append(t[2])
         tokens = self.tokenizer(phonemes).long()
         styles = torch.tensor(styles).long()
 
@@ -129,7 +129,6 @@ class SuperVoice(torch.nn.Module):
         end = round(interval[1] // phoneme_duration)
         start = min(max(0, start), seq_len - 1)
         end = min(max(0, end), seq_len - 1)
-        print(start, end, seq_len)
 
         # Create mask
         mask = torch.zeros((seq_len)).bool().to(device)

supervoice/model_style.py
@@ -15,7 +15,10 @@ def resolve_style(config, src, durations):
 
     # Make continuous and normalize
     src = _convert_to_continuous_f0(src)
-    src = (src - torch.mean(src)) / torch.std(src)
+    src = (src - torch.mean(src))
+    src_std = torch.std(src)
+    if src_std != 0: # If std is zero, we will get NaNs
+        src = src / torch.std(src)
     
     # Convert to log scale and normalize
     src = torch.clamp(src, config.tokenizer_style.pitch_min, config.tokenizer_style.pitch_max)

utils/dataset.py
@@ -168,48 +168,54 @@ def get_aligned_dataset_loader(names, voices, max_length, workers, batch_size, t
             return len(self.files)        
         def __getitem__(self, index):
 
-            # Spectogram
-            audio = torch.load(self.files[index])
-            audio = audio.transpose(0, 1) # Reshape audio (C, T) -> (T, C)
+            try:
 
-            # Styles
-            style = torch.load(self.styles[index])
+                # Spectogram
+                audio = torch.load(self.files[index])
+                audio = audio.transpose(0, 1) # Reshape audio (C, T) -> (T, C)
 
-            # Phonemes
-            aligned_phonemes = compute_alignments(config, self.textgrid[index], style, audio.shape[0])
+                # Styles
+                style = torch.load(self.styles[index])
 
-            # Unwrap phonemes
-            phonemes = []
-            styles = []
-            for t in aligned_phonemes:
-                for i in range(t[1]):
-                    phonemes.append(t[0])
-                    styles.append(t[2])
-            if len(phonemes) != audio.shape[0]:
-                raise Exception("Phonemes and audio length mismatch: " + str(len(phonemes)) + " != " + str(audio.shape[0]) + " in " + self.files[index])
+                # Phonemes
+                aligned_phonemes = compute_alignments(config, self.textgrid[index], style, audio.shape[0])
 
-            # Length
-            l = len(phonemes)
-            offset = 0
-            if l > max_length:
-                l = max_length
-                offset = random.randint(0, len(phonemes) - l)
+                # Unwrap phonemes
+                phonemes = []
+                styles = []
+                for t in aligned_phonemes:
+                    for i in range(t[1]):
+                        phonemes.append(t[0])
+                        styles.append(t[2])
+                if len(phonemes) != audio.shape[0]:
+                    raise Exception("Phonemes and audio length mismatch: " + str(len(phonemes)) + " != " + str(audio.shape[0]) + " in " + self.files[index])
+
+                # Length
+                l = len(phonemes)
+                offset = 0
+                if l  > max_length:
+                    l = max_length
+                    offset = random.randint(0, len(phonemes) - l)
         
-            # Cut to size
-            phonemes = phonemes[offset:offset+l]
-            styles = styles[offset:offset+l]
-            audio = audio[offset:offset+l]
+                # Cut to size
+                phonemes = phonemes[offset:offset+l]
+                styles = styles[offset:offset+l]
+                audio = audio[offset:offset+l]
 
-            # Tokenize
-            phonemes = tokenizer(phonemes)
-            styles = torch.tensor(styles).long()
+                # Tokenize
+                phonemes = tokenizer(phonemes)
+                styles = torch.tensor(styles).long()
 
-            # Cast
-            if dtype is not None:
-                audio = audio.to(dtype)
+                # Cast
+                if dtype is not None:
+                    audio = audio.to(dtype)
 
-            # Outputs
-            return phonemes, styles, audio
+                # Outputs
+                return phonemes, styles, audio
+            except Exception as e:
+                print("Error in file: " + self.files[index])
+                print(e)
+                raise e
 
     # Create dataset
     dataset = AlignedDataset(tg, files, styles)